c02566900e46ae05f42225ed21119cdc3777b48b
arsalanjabbari <arsalanjabbari2002@gmail.com>
2023-07-03 18:17:47 +0330

    Checkout libraries and sigmoid correctness.

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+# Need for defining sigmoid for logistic regression!
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv('Data/diabetes.csv')
 X = data.iloc[:, :-1].values
 y = data.iloc[:, -1].values
 
-from sklearn.model_selection import train_test_split
-
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
-
-# Feature scaling
-from sklearn.preprocessing import StandardScaler
 
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
