9873b65219ef2e0a2db68ad8b92b326eda821bba
arsalanjabbari <arsalanjabbari2002@gmail.com>
2023-07-03 18:08:37 +0330

    Initialize and first try implementation for main.py.

---
+++ main.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('Data.diabetes.csv')
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_val = sc.transform(X_val)
+
+
+# Define the logistic regression model
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+class LogisticRegression:
+    def __init__(self, learning_rate=0.01, num_iterations=1000):
+        self.learning_rate = learning_rate
+        self.num_iterations = num_iterations
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        num_samples, num_features = X.shape
+        self.weights = np.zeros(num_features)
+        self.bias = 0
+
+        # Gradient descent
+        for i in range(self.num_iterations):
+            linear_model = np.dot(X, self.weights) + self.bias
+            y_pred = sigmoid(linear_model)
+            dw = (1 / num_samples) * np.dot(X.T, (y_pred - y))
+            db = (1 / num_samples) * np.sum(y_pred - y)
+
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+
+    def predict(self, X):
+        linear_model = np.dot(X, self.weights) + self.bias
+        y_pred = sigmoid(linear_model)
+        y_pred_class = [1 if i > 0.5 else 0 for i in y_pred]
+        return y_pred_class
+
+
+# Train the logistic regression model
+model = LogisticRegression(learning_rate=0.1, num_iterations=1000)
+model.fit(X_train, y_train)
+
+# Evaluate the model on the validation set
+y_pred = model.predict(X_val)
+accuracy = np.sum(y_pred == y_val) / len(y_val)
+print('Accuracy:', accuracy)
