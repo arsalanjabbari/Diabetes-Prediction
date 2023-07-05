8d9a396531c92e4c0ce8851d96d6dcc4d02ddd17
arsalanjabbari <arsalanjabbari2002@gmail.com>
2023-07-05 09:26:24 +0330

    Good Job!

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Need to get input from user. (third phase)
 from GUI import values_list
 
 # Need for defining sigmoid for logistic regression!
@@ -10,8 +11,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+# Need for showing user if he/she has diabetes or not.
+import tkinter as tk
+from tkinter import ttk
+
 # Load the dataset.
-data = pd.read_csv('Data/diabetes.csv')
+data = pd.read_csv('Data/diabetes-data-2.csv')
 X = data.iloc[:, :-1].values
 y = data.iloc[:, -1].values
 
@@ -23,11 +28,9 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_val = sc.transform(X_val)
 
-
 # Define Sigmoid model.
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
-
 
 # Define the Logistic Regression model via Sigmoid.
 class LogisticRegression:
@@ -52,12 +55,12 @@ class LogisticRegression:
             self.weights -= self.learning_rate * dw
             self.bias -= self.learning_rate * db
 
+    # Prediction method for validation part!
     def predict(self, X):
         linear_model = np.dot(X, self.weights) + self.bias
         y_pred = sigmoid(linear_model)
         y_pred_class = [1 if i > 0.5 else 0 for i in y_pred]
         return y_pred_class
-
 
 # Train the logistic regression model
 model = LogisticRegression(learning_rate=0.1, num_iterations=1000)
@@ -67,6 +70,38 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_val)
 
 y_test = model.predict([values_list])
-print(y_test)
+
 accuracy = (np.sum(y_pred == y_val) / len(y_val)) * 100
 print('Accuracy:', accuracy, '%')
+
+# Output GUI part!
+def show_result(result):
+    if result == 1:
+        result_label.config(text="You have diabetes.", font=("Arial", 14, "bold"))
+    else:
+        result_label.config(text="You don't have diabetes.", font=("Arial", 14, "bold"))
+    ok_button.pack(pady=10)
+
+def close_window():
+    window.destroy()
+
+# Create the main window
+window = tk.Tk()
+window.title("Diabetes Checker")
+window.geometry("300x150")
+
+# Create the label to display the result
+result_label = ttk.Label(window, text="", font=("Arial", 12))
+result_label.pack(pady=20)
+
+# Create the "OK" button
+ok_button = ttk.Button(window, text="OK", command=close_window)
+
+# Server-side input (0 for no diabetes, 1 for diabetes)
+server_input = 1
+
+# Show the result based on the server input
+show_result(y_test)
+
+# Start the tkinter event loop
+window.mainloop()
